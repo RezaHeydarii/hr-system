d1695b9bb4f586d5c13609c98cff7235d639d47d
ReZa Heydari <re7798@gmail.com>
2022-02-11 19:30:04 +0330

    feat:base style added base on figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      i: "var(--color-i)",
+      ii: "var(--color-ii)",
+      iii: "var(--color-iii)",
+      iv: "var(--color-iv)",
+      text: {
+        dark: "var(--color-text-dark)",
+        light: "var(--color-text-light)",
+      },
+      greys: {
+        4: "var(--color-grey-4)",
+        6: "var(--color-grey-6)",
+      },
+    },
   },
   plugins: [],
 };
